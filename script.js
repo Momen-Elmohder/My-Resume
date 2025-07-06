6dd12f21fa2895318e38630ddb6cfb976aed5fcf
Momen Elmohder <moamenahmed25se@gmail.com>
2025-07-06 18:16:39 +0300

    make it responsive and add js

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-// function toggleMenu(){
-//     const menu = document.querySelector('.menu-links');
-//     const icon = document.querySelector('.burger-icons');
-//     menu.classList.toggle("open");
-//     icon.classList.toggle("open");
-// }
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", function() {
+    header.classList.toggle("sticky",this.window.scrollY > 120);
+})
+let menu = document.querySelector("#menu-icon");
+let navlist = document.querySelector('.navlist');
+
+menu.onclick = () =>{
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+    menu.remove.toggle('bx-x');
+    navlist.remove.toggle('active');
+}
